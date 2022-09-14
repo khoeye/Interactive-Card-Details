6596ed2aba39a44c4d5988953fb544ed2fc49901
khoeye <kyle.hoeye@gmail.com>
2022-09-14 00:40:11 -0500

    Added components, Bootstrap, and pictures

--- interactive-card/src/App.js
+++ interactive-card/src/App.js
@@ -1,17 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <body>
-        <Container fluid>
+        <Container>
           <Row>
-            <Col className="">
-            TEST
+            <Col sm={12} md={4} className="col_card">
+              TEST
             </Col>
-            <Col>
+            <Col sm={12} md={8} className="col_data">
               TEST
             </Col>
           </Row>
