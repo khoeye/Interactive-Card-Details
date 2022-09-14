d95659e2d48d122095388ae7e5428f3faab45cff
khoeye <kyle.hoeye@gmail.com>
2022-09-13 23:30:58 -0500

    Added Bootstrap

--- interactive-card/src/App.js
+++ interactive-card/src/App.js
@@ -1,10 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>Hello world</p>
-      </header>
+      <body>
+        <Container fluid>
+          <Row>
+            <Col className="">
+            TEST
+            </Col>
+            <Col>
+              TEST
+            </Col>
+          </Row>
+        </Container>
+      </body>
     </div>
   );
 }
