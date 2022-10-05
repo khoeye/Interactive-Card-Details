59fd0c04ef093fa78c792b36d94990be1d919179
khoeye <kyle.hoeye@gmail.com>
2022-10-04 22:25:49 -0500

    Revert "Revert "CSS Fix and Form add""
    This reverts commit fa1585b75944d383de2fb5a5b91ae60ba722b68e.

--- interactive-card/src/Component/CardContainer/CardContainer.js
+++ interactive-card/src/Component/CardContainer/CardContainer.js
@@ -1,10 +1,11 @@
 import React from "react";
+import CardForm from "../CardForm/CardForm";
 import './CardContainer.css'
 
 const CardContainer = (props) =>{
     return(
         <div className="cardContainer">
-            <p>Card Container</p>
+            <CardForm/>
         </div>
     )
 } 
