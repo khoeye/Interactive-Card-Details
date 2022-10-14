a060c6299fd5fb03840267e9d1adff03e7a072a4
khoeye <kyle.hoeye@gmail.com>
2022-10-14 13:53:58 -0500

    added state logic for all form data

--- interactive-card/src/Component/CardForm/CardForm.js
+++ interactive-card/src/Component/CardForm/CardForm.js
@@ -5,25 +5,45 @@ const CardForm = (props) => {
 
     //0=Incorrect Value entered, 1=No Value Entered, 2=ValidValue Entered
     const [cardNumberEmpty, setCardNumberEmpty] = useState(1)
-    const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000")
-    props.onAddData(cardNumber)
+    const [cardForm, setCardForm] = useState(
+        {  
+     }
+    )
+    props.onAddData(cardForm)
 
+    const cardNameHandler = (event) =>{
+        setCardForm(prevState =>{
+            return ({...prevState, name: event.target.value})
+    })}
     const cardNumberHandler = (event) =>{
-        setCardNumber(event.target.value)
-    }
+        setCardForm(prevState =>{
+            return ({...prevState, card: event.target.value})
+    })}
+    const cardexpMMHandler = (event) =>{
+        setCardForm(prevState =>{
+            return ({...prevState,expMM: event.target.value})
+    })}
+    const cardexpYYHandler = (event) =>{
+        setCardForm(prevState =>{
+            return ({...prevState,expYY: event.target.value})
+    })}
+    const cardCvcHandler = (event) =>{
+        setCardForm(prevState =>{
+            return ({...prevState,cvc: event.target.value})
+    })}
 
     return (
     <div className="formContainer">
             <form>
                 <p>CARDHOLDER NAME</p>
-                    <input type={"text"} placeholder={"e.g. Jane Appleseed"}/>
+                    <input type={"text"} placeholder={"e.g. Jane Appleseed"} onChange={cardNameHandler}/>
                 <p>CARD NUMBER</p>
                 <input type={"text"} placeholder={"e.g. 1234 5678 9123 0000"} onChange={cardNumberHandler}/>
                 <p>EXP. DATE (MM/YY)</p>
-                <input type={"text"} placeholder={'MM'}/>
-                <input type={"text"} placeholder={'YY'}/>
+                <input type={"text"} placeholder={'MM'} onChange={cardexpMMHandler}/>
+                <input type={"text"} placeholder={'YY'} onChange={cardexpYYHandler}/>
                 <p>CVC</p>
-                <input type={"text"} placeholder={'e.g. 123'}/>
+                <input type={"text"} placeholder={'e.g. 123'} onChange={cardCvcHandler}/>
                 <button>Confirm</button>
             </form>
         </div>
--- interactive-card/src/Component/FrontCard/FrontCard.js
+++ interactive-card/src/Component/FrontCard/FrontCard.js
@@ -6,13 +6,18 @@ import './FrontCard.css'
 
 const FrontCard = (props) =>{
 
+
     return(
         <div className="frontCardContainer">
             <div className={"cardIcon"}>
                 
             <CardIcon/>
             </div>
-            <h1 className="cardNumber">{props.e}</h1>
+            <h1 className="cardNumber">{props.e.card}</h1>
+            <p>{props.e.name}</p> 
+            <span>
+                <p>{props.e.expMM}/{props.e.expYY}</p>
+            </span>
         </div>
     )
 } 
